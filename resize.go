89a497214bbc33fed8f4b4b2e2f0a7ed4f5ffb9d
冠勋 王 <wardon.wang@webfoss.com>
2019-04-03 18:13:08 +0800

    update

--- resize.go
+++ resize.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"image"
+	// "image/color"
 )
 
 func ScaleImage(img image.Image, r float64) int {
 	// var dx, dy int = GetImageBounds(img)
+	// var w, h int = 0, 0
+	// w = int(float64(dx) * r)
+	// h = int(float64(dy) * r)
+	// var canvas image.RGBA = CreateCanvas(w, h)
+	// for i := 0; i < w; i++ {
+	// 	for j := 0; j < h; j++ {
+	// 		r, g, b, a := GetUint8RGBA(img, i, j)
+	// 		canvas.SetRGBA(i, j, color)
+	// 	}
+	// }
 	return 1
 }
--- rgba.go
+++ rgba.go
@@ -4,16 +4,19 @@ import (
 	"image"
 )
 
+// GetIntRGBA ...
 func GetIntRGBA(img image.Image, x int, y int) (int, int, int, int) {
 	r, g, b, a := img.At(x, y).RGBA()
 	return int(r >> 8), int(g >> 8), int(b >> 8), int(a >> 8)
 }
 
+// GetUint8RGBA ...
 func GetUint8RGBA(img image.Image, x int, y int) (uint8, uint8, uint8, uint8) {
 	r, g, b, a := img.At(x, y).RGBA()
 	return uint8(r >> 8), uint8(g >> 8), uint8(b >> 8), uint8(a >> 8)
 }
 
+// GetFloat64RGBA ...
 func GetFloat64RGBA(img image.Image, x int, y int) (float64, float64, float64, float64) {
 	r, g, b, a := img.At(x, y).RGBA()
 	return float64(r >> 8), float64(g >> 8), float64(b >> 8), float64(a >> 8)
